fec4d612aa3ff72de942a06a4ea4010b10e0c7c0
lapaduga <47709041+lapaduga@users.noreply.github.com>
2020-10-09 12:51:55 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	//aside menu
+	$('.aside__hide').click(function (event) {
+		$('.aside, .aside__hide, .aside__title, .aside__menu, .main, .profile, .profile__image, .profile__technical, .decor, .profile__name, .profile__position, .profile__tests-done, .profile__finished-tests').toggleClass('passive');
+	});
+
+	//checkbox
+	$.each($('.test__form-option'), function(index, val){
+		if($(this).find('input').prop('checked')==true){
+			$(this).addClass('active');
+		}
+	});
+	$(document).on('click', '.test__form-option', function(event){
+		if ($(this).hasClass('active')) {
+			$(this).find('input').prop('checked', false);
+		} else {
+			$(this).find('input').prop('checked', true);
+		}
+		$(this).toggleClass('active');
+		return false;
+	});
+});
