c35f26406710d2061f51ef26c923ff26d4b79c16
lapaduga <deniskotov29042015@gmail.com>
2020-10-09 18:20:23 +0500

    f

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	//aside menu
 	$('.aside__hide').click(function (event) {
-		$('.aside, .aside__hide, .aside__title, .aside__menu, .main, .profile, .profile__image, .profile__technical, .decor, .profile__name, .profile__position, .profile__tests-done, .profile__finished-tests').toggleClass('passive');
+		$('.aside, .aside__hide, .aside__title, .aside__link, .aside__title-passive, .main, .profile, .profile__image, .profile__technical, .decor, .profile__name, .profile__position, .profile__tests-done, .profile__finished-tests, .footer').toggleClass('passive');
 	});
 
 	//checkbox
