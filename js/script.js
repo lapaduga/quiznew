69b18053483844182c59aabbd8b1ac6ac368e93c
lapaduga <deniskotov29042015@gmail.com>
2020-10-10 21:56:55 +0500

    f

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
 	//aside menu
 	$('.aside__hide').click(function (event) {
-		$('.aside, .aside__hide, .aside__title, .aside__link, .aside__title-passive, .main, .profile, .profile__image, .profile__technical, .decor, .profile__name, .profile__position, .profile__tests-done, .profile__finished-tests, .footer').toggleClass('passive');
+		$('.aside, .aside__hide, .aside__title, .aside__link, .main, .profile, .profile__image, .profile__technical, .decor, .profile__name, .profile__position, .profile__tests-done, .profile__finished-tests, .footer').toggleClass('passive');
 	});
 
 	//header burger menu
 	$('.header__burger, .close-menu-area, .header__link').click(function (event) {
 		$('.header__burger, .header__menu, .header__menu-bg').toggleClass('active');
 		$('body').toggleClass('lock');
+	});
+
+	//header__profile
+	$('.header__profile-image').click(function (event) {
+		$('.quiz__body, .header__profile, .footer').toggleClass('show');
 	});
 
 	//checkbox
