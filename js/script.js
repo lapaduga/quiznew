bc66c427901b46fda184622ba26eafc12b265ad9
lapaduga <deniskotov29042015@gmail.com>
2020-10-13 02:07:12 +0500

    n

--- js/script.js
+++ js/script.js
@@ -67,4 +67,18 @@ $(document).ready(function () {
 	$('.test__help, .test__hint-close, .test__image-mobile').on('click', function () {
 		$('.test__board, .test__hint, .test__legend, .test__hint-aware-mobile').toggleClass('switch');
 	});
+
+		//popup-wrong & popup-right !!! Клики навесил просто для демонстрации, чтобы было ясно, что попап работает, меняйте как вам надо !!!
+		$('.answer-button-check').click(function (event) {
+			$('.popup-wrong').addClass('open');
+			$('body').addClass('lock');
+		});
+		$('.test__form-option').click(function (event) {
+			$('.popup-right').addClass('open');
+			$('body').addClass('lock');
+		});
+		$('.popup__ok, .popup__close-area').click(function (event) {
+			$('.popup').removeClass('open');
+			$('body').removeClass('lock');
+		});
 });
